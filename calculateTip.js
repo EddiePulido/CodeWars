abebacaa6cdf032e5fce0cf1e89820556bcf962b
Eddie <eddiepulidodev@gmail.com>
2022-06-21 00:26:36 -0700

    June21st

---
+++ calculateTip.js
@@ -0,0 +1,17 @@
+function calculateTip(amount, rating) {
+  let r = rating.toLowerCase()
+  
+  switch(r){
+      case 'excellent':
+        return Math.ceil(amount * .2)
+      case 'great':
+        return Math.ceil(amount * .15)
+      case 'good':
+        return Math.ceil(amount * .1)
+      case 'poor':
+        return Math.ceil(amount * .05)
+      case 'terrible':
+        return 0
+      default:
+        return 'Rating not recognised'
+  }
--- maxSub.js
+++ maxSub.js
@@ -1,17 +1,14 @@
 var maxSequence = function(arr){
-  console.log({arr})
   if(!arr.length) return 0
   
-  const best = [arr[0]]
+  let best = arr[0]
   
   let sum = arr[0]
   
   for(let i = 1; i < arr.length; i++){
     sum = Math.max(sum + arr[i], arr[i])
-    best.push(sum)
+    if(sum > best) best = sum
   }
   
-  console.log({best})
-  
-  return Math.max(...best,0)
+  return Math.max(best, 0)
 }
