a5c2293cf64ef1f1aef64f4c38bfd8f03b039dd5
Eddie <eddiepulidodev@gmail.com>
2022-07-10 22:08:01 -0700

    codewars

---
+++ rooms.js
@@ -0,0 +1,18 @@
+// Add rooms here
+var rooms = {
+  room1 : {
+    name : 'Room1',
+    description: 'room',
+    completed: false
+  },
+  room2 : {
+    name : 'Room2',
+    description: 'room',
+    completed: false
+  },
+  room3 : {
+    name : 'Room3',
+    description: 'room',
+    completed: false
+  }
+}
