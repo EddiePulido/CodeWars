4ffcd0d68c7b0f767cd2ff4c5baf119e6e483536
Eddie <eddiepulidodev@gmail.com>
2022-07-23 01:53:54 -0700

    codewars

---
+++ fuelPrice.js
@@ -0,0 +1,12 @@
+function Dog (breed) {
+  this.breed = breed;
+}
+
+var snoopy = new Dog("Beagle");
+
+snoopy.bark = function() {
+  return "Woof";
+};
+
+var scoobydoo = new Dog("Great Dane");
+scoobydoo.bark = () => 'Woof';
