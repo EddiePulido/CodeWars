5d3cfc866a22e805906ba4796e1d4fe83077f12e
Eddie <eddiepulidodev@gmail.com>
2022-07-24 02:51:11 -0700

    codewars

---
+++ unique.js
@@ -0,0 +1,9 @@
+function solve(a,b){
+  let result = ''
+  
+  for(let i = 0; i < a.length; i++) if(!b.includes(a[i])) result += a[i]
+  
+  for(let i = 0; i < b.length; i++) if(!a.includes(b[i])) result += b[i]
+  
+  return result
+};
