ad80074e25dd89aa6fd2fcc345f693d827b938a4
Eddie <eddiepulidodev@gmail.com>
2022-08-02 14:11:42 -0700

    codewars

---
+++ removeDup.js
@@ -0,0 +1,9 @@
+function removeDuplicateWords (s) {
+  let result = []
+  
+  s.split(' ').forEach(e => {
+    if(!result.includes(e)) result.push(e)
+  })
+                      
+  return result.join(' ')
+}
