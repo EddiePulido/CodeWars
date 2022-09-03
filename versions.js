e946bb73dc93911e2f9b7f9eef9607092ff35c9f
Eddie <eddiepulidodev@gmail.com>
2022-09-03 01:36:32 -0700

    codewars

---
+++ versions.js
@@ -0,0 +1,13 @@
+function compareVersions (version1, version2) {
+  let v1 = version1.split('.').map(e=>+e)
+  let v2 = version2.split('.').map(e=>+e)
+
+  const len = Math.max(v1.length, v2.length)
+  
+  for(let i = 0; i < len; i++){
+    if(v1[i] < v2[i]) return false
+    if(v1[i] > v2[i]) return true
+    if(!v1[i] && v1[i] != 0) return false
+  }
+  return true
+}
