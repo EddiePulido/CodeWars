ae99e5f3390a1a5ac43018f2e4116efa69c01424
Eddie <eddiepulidodev@gmail.com>
2022-09-06 13:09:33 -0700

    codewars

---
+++ caffeine.js
@@ -0,0 +1,6 @@
+function caffeineBuzz(n){
+  if(n % 3 === 0 && n % 4 === 0) return 'CoffeeScript'
+  if(n % 2 === 0 && n % 3 === 0) return 'JavaScript'
+  if(n % 3 === 0) return 'Java'
+  return "mocha_missing!"
+}
