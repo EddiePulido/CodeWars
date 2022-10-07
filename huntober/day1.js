37fbe46f5717562cb505c123651e0d539c3945f0
Eddie <eddiepulidodev@gmail.com>
2022-10-07 02:02:34 -0700

    codewars

---
+++ huntober/day1.js
@@ -0,0 +1,32 @@
+const day1 = str => {
+  const code = {
+    '0' : 'O',
+    '1' : 'I',
+    '2' : 'Z',
+    '3' : 'E',
+    '4' : 'h',
+    '5' : 'S',
+    '6' : 'G',
+    '7' : 'L',
+    '8' : 'B',
+    '9' : 'q',
+    'O' : '0',   
+    'I' : '1',  
+    'Z' : '2',  
+    'E' : '3',  
+    'h' : '4',  
+    'S' : '5',  
+    'G' : '6',  
+    'L' : '7',  
+    'B' : '8',  
+    'q' : '9',   
+  }
+
+  return str.split('').map(x => code[x] ? code[x] : x ).join('')
+}
+let str1 = "PR0-T1P #hqB: 1T'5 N1C3 T0 5AY H3770."
+let str2 = "PRO-TIP #498: IT'S NICE TO SAY HELLO."
+
+
+console.log(day1(str1))
+console.log(day1(str2))
