b14c90cf95381ef6c05655bdeb666f9323f2ac42
Eddie <eddiepulidodev@gmail.com>
2022-10-13 21:44:41 -0700

    codewars

--- dupEn.js
+++ dupEn.js
@@ -3,6 +3,6 @@ function duplicateEncode(word){
   let hash = [...word].reduce((a, c) => {
     a[c] = ++a[c] || 1
     return a}, {})
-  
+   
   return [...word].map(e => hash[e] === 1 ? '(' : ')').join('')
 }
