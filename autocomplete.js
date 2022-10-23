b6b1239c300e1917eedff8c4987f903925efd93f
Eddie <eddiepulidodev@gmail.com>
2022-10-22 23:59:13 -0700

    codewars

--- autocomplete.js
+++ autocomplete.js
@@ -2,4 +2,4 @@ function autocomplete(input, dictionary){
   const alpha = "abcdefghijklmnopqrstuvwxyz"
   input = input.split('').filter(e => alpha.includes(e)).join('')
   return dictionary.filter(e => e.slice(0,input.length).toLowerCase() === input.toLowerCase()).slice(0,5)
-}
+} 
