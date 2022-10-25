19fd98ac5711ad4191d82898b0c05da1cfba02ab
Eddie <eddiepulidodev@gmail.com>
2022-10-24 21:43:18 -0700

    codewars

--- major2.js
+++ major2.js
@@ -1,5 +1,5 @@
 const majorityElement = (nums) => {
-  let result, count = 0
+  let result, count = 0 
   for(let i = 0; i <nums.length;i++){
       if(count == 0) (ans = nums[i],count = 1)
       else if (ans == nums[i]) count++ 
