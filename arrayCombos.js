0bba76a93571c2ac693454bfd6054da0cb68eda4
Eddie <eddiepulidodev@gmail.com>
2022-10-26 20:34:21 -0700

    codewars

---
+++ arrayCombos.js
@@ -0,0 +1,8 @@
+function solve(arr) {
+  let total = 1
+  for(a of arr){
+    total *= (new Set(a)).size
+    console.log(total)
+  }
+  return total
+};
---
+++ dupEn.js
@@ -0,0 +1,8 @@
+function duplicateEncode(word){
+  word = word.toLowerCase()
+  let hash = [...word].reduce((a, c) => {
+    a[c] = ++a[c] || 1
+    return a}, {})
+  
+  return [...word].map(e => hash[e] === 1 ? '(' : ')').join('')
+}
