4908ba5cbb77aeef1065b3327008507eed472aae
Eddie <eddiepulidodev@gmail.com>
2022-10-29 17:39:31 -0700

    codewars

---
+++ addTwoLink.js
@@ -0,0 +1,48 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+ var addTwoNumbers = function(l1, l2) {
+    
+  let carry = 0
+  let dummy = new ListNode()
+  let dhead = dummy
+  
+  while(l1 || l2){
+      let v1 = l1 ? l1.val : 0
+      let v2 = l2 ? l2.val : 0
+      let sum = v1 + v2 + carry
+      if(sum >= 10){
+          sum = sum % 10
+          carry = 1
+      }else{
+          carry = 0
+      }
+      
+      let newNode = new ListNode(sum)
+      dummy.next = newNode
+      dummy = dummy.next
+      
+      if(l1){
+          l1 = l1.next
+      }
+      if(l2){
+          l2 = l2.next
+      }
+  }
+  
+  if(carry){
+      const node = new ListNode(1)
+      dummy.next = node
+  }
+  
+  return dhead.next
+};
