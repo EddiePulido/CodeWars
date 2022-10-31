3e3b423b0ec3fa71535c677dbe01d825255fa543
Eddie <eddiepulidodev@gmail.com>
2022-10-31 00:05:49 -0700

    codewars

---
+++ add22.js
@@ -0,0 +1,36 @@
+var addTwoNumbers = function(l1, l2) {
+    
+  let carry = 0
+  let dummy = new ListNode()
+  let dhead = dummy
+  
+  while(l1 || l2){
+      let v1 = l1 ? l1.val : 0
+      let v2 = l2 ? l2.val : 0
+      let sum = v1 + v2 + carry
+      if(sum >= 10){
+          sum = sum % 10
+          carry = 1
+      }else{
+          carry = 0
+      }
+      
+      let newNode = new ListNode(sum)
+      dummy.next = newNode
+      dummy = dummy.next
+      
+      if(l1){
+          l1 = l1.next
+      }
+      if(l2){
+          l2 = l2.next
+      }
+  }
+  
+  if(carry){
+      const node = new ListNode(1)
+      dummy.next = node
+  }
+  
+  return dhead.next
+};
