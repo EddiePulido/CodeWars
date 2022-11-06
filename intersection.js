476d02e59c88e01ee834c9ea22f92b13640ce067
Eddie <eddiepulidodev@gmail.com>
2022-11-06 11:57:11 -0800

    codewars

---
+++ intersection.js
@@ -0,0 +1,8 @@
+var getIntersectionNode = function(headA, headB) {
+    let pA = headA, pB = headB;
+    while (pA !== pB) {
+        pA = pA === null ? headB : pA.next;
+        pB = pB === null ? headA : pB.next;
+    }
+    return pA;
+}
