46c07c58c3e0acdfb152a23b6fab539625d45ef9
Eddie <eddiepulidodev@gmail.com>
2022-11-12 14:08:21 -0800

    codewars

---
+++ sumStringNum.js
@@ -0,0 +1,20 @@
+var SequenceSum = (function() {
+  function SequenceSum() {}
+
+  SequenceSum.showSequence = function(count) {
+    let result = []
+    
+    for(let i = 0; i <= count; i++){
+      result.push(i)
+    }
+    let sum = result.reduce((acc,c) => acc + c, 0)
+    
+    let end = result.join('+') + ' = ' + sum
+    if(end === '0 = 0') return '0=0'
+    if(end === ' = 0') return count+'<0'
+    return end
+  };
+
+  return SequenceSum;
+
+})();
