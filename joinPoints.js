18d910afa3de6a6fd614f030eec1d3d5e4276b5f
Eddie <eddiepulidodev@gmail.com>
2022-11-17 18:52:21 -0800

    codewars

--- joinPoints.js
+++ joinPoints.js
@@ -4,7 +4,7 @@ function computeJoinPoint(s1, s2){
     if(s1 < s2)
       s1 = s1.toString().split('').reduce((a,c) => a + +c, s1)
     else
-      s2 = s2.toString().split('').reduce((a,c) => a + +c, s2)
+      s2 = s2.toString().split('').reduce((a,c) => a + +c, s2) 
   }
   
   return s1
