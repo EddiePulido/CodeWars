9c144a5378c658ff927008408aa596981c41bc11
Eddie <eddiepulidodev@gmail.com>
2022-11-21 12:17:59 -0800

    codewars

---
+++ consecGamma.js
@@ -0,0 +1,10 @@
+const removeConsecutiveDuplicates = s => {
+  let arr = s.split(' ')
+  let result = []
+  
+  for(let i = 0; i < arr.length; i++){
+    if(arr[i] !== arr[i + 1]) result.push(arr[i])
+  }
+  
+  return result.join(' ')
+}
