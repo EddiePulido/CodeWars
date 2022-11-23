0aa54223fe930e13b9f9c27f80f6dfe383dcc043
Eddie <eddiepulidodev@gmail.com>
2022-11-23 12:39:13 -0800

    codewars

---
+++ simpStr.js
@@ -0,0 +1,15 @@
+function solve(s){
+  return [...s].reduce((a,c) => {
+    let isChar = c.toLowerCase() !== c.toUpperCase()
+    
+    if(isChar && c === c.toUpperCase()) 
+      a[0]++
+    else if(isChar && c === c.toLowerCase()) 
+      a[1]++
+    else if(Number.isInteger(+c)) 
+      a[2]++
+    else 
+      a[3]++
+    return a
+  }, [0,0,0,0])
+}
