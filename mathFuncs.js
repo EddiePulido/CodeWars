d7c9549c1abb25adb793dfde10d91bbffa5cb1bf
Eddie <eddiepulidodev@gmail.com>
2022-12-10 14:48:12 -0800

    codewars

---
+++ mathFuncs.js
@@ -0,0 +1,21 @@
+
+Math.ceil = function(n) {
+  if(!n.toString().includes('.')) return n 
+  return +n.toString().split('.')[0] + 1
+};
+
+Math.floor = function(n) {
+  return +n.toString().split('.')[0]
+};
+
+Math.round = function(n) {
+  let str = n.toString()
+  if(str.includes('.')){
+    let num = str.split('.')
+    console.log(num[1])
+    if(+num[1][0] >= 5) return +num[0] + 1
+    return +num[0]
+  }
+  
+  return n
+};
