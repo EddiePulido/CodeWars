08de8172583301a8e25fc126fb455127d1ecf16b
Eddie <eddiepulidodev@gmail.com>
2022-12-19 01:41:44 -0800

    codewars

--- maxyMiny.js
+++ maxyMiny.js
@@ -5,7 +5,7 @@ function max(){
 }
 
 function min(){
-  let arr = Array.from(arguments).flat(1000)
+  let arr = Array.from(arguments).flat(1000) 
   if(arr.length === 0) return +0
   return +arr.reduce((a,c) => +c < a || isNaN(+c) ? +c : a, +arr[0])
 }
