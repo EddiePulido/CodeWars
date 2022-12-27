9f35fdad18cb88133893321bb855ee9ff477d481
Eddie <eddiepulidodev@gmail.com>
2022-12-27 11:42:49 -0800

    codewars

---
+++ bald.js
@@ -0,0 +1,17 @@
+function bald(x){
+  const count = [...x].filter(e => e === '/').length
+  const c = 'Careless!'
+  
+  let hash = {
+    0 : 'Clean!',
+    1 : 'Unicorn!',
+    2 : 'Homer!',
+    3 : c,
+    4 : c,
+    5 : c
+  }
+  
+  const phrase = hash[count] || 'Hobo!'
+  
+  return ['-'.repeat(x.length), phrase]
+}
