7bb08dc38fe566778d75bb3b2d24d5d5b4bd4d90
Eddie <eddiepulidodev@gmail.com>
2023-01-01 20:56:39 -0800

    codewars

---
+++ clash5.js
@@ -0,0 +1,11 @@
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+
+const n = readline()
+
+// Write an answer using console.log()
+// To debug: console.error('Debug messages...');
+
+console.log(+n / n.length);
