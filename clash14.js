34b42701e059bbae58d1e3815bd252ec53895dac
Eddie <eddiepulidodev@gmail.com>
2023-01-06 02:18:43 -0800

    codewars

---
+++ clash14.js
@@ -0,0 +1,11 @@
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+
+const x = parseInt(readline());
+
+// Write an answer using console.log()
+// To debug: console.error('Debug messages...');
+
+console.log(x * Math.abs(x).toString().length);
