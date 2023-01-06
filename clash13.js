9eed5667be35ddfdc02be088e7155bcae274e62f
Eddie <eddiepulidodev@gmail.com>
2023-01-06 02:11:40 -0800

    codewars

---
+++ clash13.js
@@ -0,0 +1,9 @@
+
+const n =readline()
+if(n.includes('6') && n.includes('8')){
+    print('Not Lucky')
+}else if(n.includes('6') || n.includes('8')){
+    print('Lucky')
+}else{
+    print('Not Lucky')
+}
