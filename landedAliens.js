09bbce395021365e43742e3b1184a27068253b21
Eddie <eddiepulidodev@gmail.com>
2023-01-14 17:35:12 -0800

    codewars

--- landedAliens.js
+++ landedAliens.js
@@ -2,8 +2,9 @@ function translateToAlienLanguage(str) {
   const vowels = 'aeiou'
   str = str.toLowerCase()
   return str.split(' ').map(word => {
-    return [...word].map(c=> vowels.includes(c) ? c + c : c)
-                    .reverse()
-                    .join('')
+    return [...word]
+            .map(c=> vowels.includes(c) ? c + c : c)
+            .reverse()
+            .join('')
   }).join(' ')
 }
