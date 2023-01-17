6361159d8732ae7a25531a49ba2e73d1761083bc
Eddie <eddiepulidodev@gmail.com>
2023-01-17 13:43:28 -0800

    codewars

---
+++ absolute.js
@@ -0,0 +1,5 @@
+///You have an integer X. Your objective is to find the absolute difference between the value X to the power 2 and (X+1) to the power 2.
+
+const X = parseInt(readline());
+print(Math.abs(X ** 2 - (X + 1) ** 2))
+
