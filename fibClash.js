ffd9488ab039a07cca080f2efb2fed990aaacfde
Eddie <eddiepulidodev@gmail.com>
2023-01-19 13:48:16 -0800

    codewars

---
+++ fibClash.js
@@ -0,0 +1,27 @@
+// You are rendering a pot plant with N number of leaves in console.
+// The first leaf will always be in the center.
+// The even numbered position leaf will be on the left of the plant.
+// The odd numbered position leaf from the 3rd on-wards will be on the right of the plant.
+
+// The leaves are counted from the top to bottom.
+
+// Example plant render with pot:
+
+//   (|)
+//    |
+//    |
+// (|)|
+//  --+
+//    |
+//    |(|)
+//    +--
+// ___|___
+// \     /
+//  \___/
+
+const leaves = parseInt(readline());
+
+
+
+
+console.log('___|___\n\\     /\n \\___/');
