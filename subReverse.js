091b1018f39677718626c6d107f2ef0c78406987
Eddie <eddiepulidodev@gmail.com>
2023-02-05 19:31:22 -0600

    codewars

---
+++ subReverse.js
@@ -0,0 +1,10 @@
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+
+const n = (readline())
+
+
+
+console.log(+n - +[...n].reverse().join(''));
