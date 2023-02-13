de375c4565bab2cc05c5008700fc959677b0fde0
Eddie <eddiepulidodev@gmail.com>
2023-02-13 01:10:47 -0800

    codewars

---
+++ coordinates.js
@@ -0,0 +1,22 @@
+// You are given the three corner points of a triangle as coordinates in the plane.
+// Find the midpoint of the triangle given.
+
+// The math:
+// A triangle's midpoint is the point with the shortest possible sum of the distance to the three corners.
+// Imagine drawing a line from any of the three corners to the middle point of the opposite side of the triangle. These three lines meet at a single point known as the triangle midpoint, geometric center, centroid, or middle point.
+// Your task is to provide the coordinates of this middle point of the triangle given.
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+for (let i = 0; i < 3; i++) {
+  var inputs = readline().split(' ');
+  const x = parseInt(inputs[0]);
+  const y = parseInt(inputs[1]);
+  // Write an action using console.log()
+  // To debug: console.error('Debug messages...');
+  console.log('Debug messages...');
+  
+  
+}
