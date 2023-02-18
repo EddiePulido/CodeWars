2a8be5e5d10b3942e6af1faa8a2863f43493f3a2
Eddie <eddiepulidodev@gmail.com>
2023-02-17 20:35:25 -0800

    codewars

---
+++ sumIntervals.js
@@ -0,0 +1,20 @@
+function sumIntervals(intervals) {
+  intervals.sort((a, b) => a[0] - b[0]);
+
+  let sum = 0;
+  let end = -Infinity;
+
+  for (let i = 0; i < intervals.length; i++) {
+    const [start, stop] = intervals[i];
+
+    if (start >= end) {
+      sum += stop - start;
+      end = stop;
+    } else if (stop > end) {
+      sum += stop - end;
+      end = stop;
+    }
+  }
+
+  return sum;
+}
