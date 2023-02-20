e957dcb57a1c797998e47166619df0c7fd121b84
Eddie <eddiepulidodev@gmail.com>
2023-02-19 22:20:22 -0800

    codewars

---
+++ wizard.js
@@ -0,0 +1,24 @@
+const N = parseInt(readline());
+
+
+
+for (let i = 0; i < N; i++) {
+    const map = {
+        w : 1,
+        i : 1,
+        z : 1,
+        a : 1,
+        r : 1,
+        d : 1
+    }
+    const names = readline().toLowerCase();
+
+    [...names].forEach(c => {
+        if(map[c]){
+            map[c]--
+        }
+    })
+
+    Object.values(map).every(e => e <= 0) ? print('Is a wizard') : print('Not a wizard')
+}
+
