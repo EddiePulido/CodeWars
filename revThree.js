15fb0bc248b2127518571b6bea2367770a84d43c
Eddie <eddiepulidodev@gmail.com>
2023-02-22 18:20:52 -0800

    codewars

---
+++ revThree.js
@@ -0,0 +1,12 @@
+const N = parseInt(readline());
+const L = parseInt(readline());
+const S = parseInt(readline());
+
+let sum = 0
+
+for(let i = 1; i <= N; i++){
+    sum += L 
+    sum += S + ((i - 1) * 2)
+}
+
+print(sum)
