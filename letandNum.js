b36c0cbb64c797e0fb2cfab5ee4e3307b3fb4543
Eddie <eddiepulidodev@gmail.com>
2023-02-27 14:24:32 -0800

    codewars

---
+++ letandNum.js
@@ -0,0 +1,9 @@
+const s = readline();
+
+let d = '0123456789'
+
+let nums = [...s].filter(e => d.includes(e)).reduce((a,c) => a + +c, 0)
+let le = [...s].filter(e => !d.includes(e)).join('')
+
+print(le)
+print(nums)
