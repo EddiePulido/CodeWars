5759183c6e334b47de3fcf571c6879e45a255bd9
Eddie <eddiepulidodev@gmail.com>
2023-03-10 02:32:32 -0800

    codewars

--- divisors2.js
+++ divisors2.js
@@ -16,3 +16,15 @@ let s = d.reduce((a,c) => a + c, 0)
 if(n > s) print('deficient')
 if(n < s) print('abundant')
 if(n===s) print('perfect')
+
+const isPrime = n => {
+    if(n < 2) return false
+    if(n === 2) return true
+    if(n % 2 === 0) return false
+
+    for(let i = 3; i < n; i++){
+        if(n % i === 0) return false
+    }
+
+    return true
+}
