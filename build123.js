29a1e36585853ea20494f74799d0ff034584ae45
Eddie <eddiepulidodev@gmail.com>
2023-03-21 02:55:38 -0700

    codewars

---
+++ build123.js
@@ -0,0 +1,21 @@
+function Node(data) {
+  this.data = data;
+  this.next = null;
+}
+
+function push(head, data) {
+  const node = new Node(data)
+  
+  if(head) node.next = head
+  
+  return node
+}
+
+function buildOneTwoThree() {
+  let chained = null
+  chained = push(chained, 3)
+  chained = push(chained, 2)
+  chained = push(chained, 1)
+  
+  return chained
+}
