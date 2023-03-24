8c4fa25ed8a7ad439fe7a0cf8dc10ba186aebaf4
Eddie <eddiepulidodev@gmail.com>
2023-03-23 21:41:50 -0700

    codewars

--- mispelled.js
+++ mispelled.js
@@ -16,3 +16,15 @@ const mispelled = (w1, w2) =>{
   
   return count <= 1
 }
+
+const mispelled = (w1, w2) =>{
+  let count = 0
+  
+  if(Math.abs(w1.length - w2.length) === 1)
+    return w1.includes(w2) || w2.includes(w1)
+  
+  for(let i = 0; i < w1.length; i++)
+    if(w1[i] !== w2[i]) count++
+  
+  return count <= 1
+}
