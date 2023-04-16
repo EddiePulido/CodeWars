2869e993cf68543298436c356f0a9866add32e9e
Eddie <eddiepulidodev@gmail.com>
2023-04-15 21:33:48 -0700

    codewars

---
+++ parseCss.js
@@ -0,0 +1,15 @@
+function parseHTMLColor(color) {
+  if(PRESET_COLORS[color.toLowerCase()]) color = PRESET_COLORS[color.toLowerCase()]
+
+  if(color.length === 4){
+    const [other,r,g,b] = [...color]
+    return {r: parseInt(r+r,16), g: parseInt(g+g,16), b: parseInt(b+b,16)}
+  }
+  
+  const r = parseInt(color.slice(1,3),16)
+  const g = parseInt(color.slice(3,5),16)
+  const b = parseInt(color.slice(5),16)
+  
+  return {r:r,g:g,b:b}
+  
+}
