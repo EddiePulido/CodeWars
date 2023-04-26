9b7faaa65274336a0b4916e0b4bb583da6f7e684
Eddie <eddiepulidodev@gmail.com>
2023-04-26 15:37:17 -0700

    codewars

--- initials.js
+++ initials.js
@@ -2,3 +2,5 @@ const initials = n => {
   const u = s => s[0].toUpperCase()
   return n.split(' ').map((e,i,a) => a[i+1] ? u(e) : u(e) + e.slice(1)).join('.')
 }
+
+const initials = n => n.split(' ').map((e,i,a) => e[0].toUpperCase() + (a[i+1] ? '' : e.slice(1))).join('.')
