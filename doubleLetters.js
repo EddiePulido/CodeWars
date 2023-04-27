7e6e1016958ac63b40bbf6272931d5ae7b31a0df
Eddie <eddiepulidodev@gmail.com>
2023-04-27 12:02:49 -0700

    codewars

--- doubleLetters.js
+++ doubleLetters.js
@@ -3,4 +3,4 @@ const S = readline();
 const filt = s => [...s].filter((e,i) => e !== s[i-1]).join('')
 
 
-print(S.split(' ').filter(word => word.toLowerCase() !== filt(word.toLowerCase())).length)
+print(S.split(' ').filter(word => word.toLowerCase() !== filt(word.toLowerCase())).length) 
