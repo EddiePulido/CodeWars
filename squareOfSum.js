082c69e47a4a3c9fccfba377c9407307d6a5a7ad
Eddie <eddiepulidodev@gmail.com>
2023-04-29 20:11:59 -0700

    codewars

---
+++ squareOfSum.js
@@ -0,0 +1,18 @@
+const N = parseInt(readline());
+for (let i = 0; i < N; i++) {
+    var inputs = readline().split(' ');
+    const a = parseInt(inputs[0]);
+    const b = parseInt(inputs[1]);
+
+    let sumOfS = a ** 2 + b ** 2
+    let squareOfS = (a + b) ** 2
+
+    if(sumOfS > squareOfS){
+        print('>')
+    }else if(sumOfS < squareOfS){
+        print('<')
+    }else{
+        print('=')
+    }
+}
+
