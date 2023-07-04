12fc9adbeb180bff1ac5e4c0dbb77c12ed7edbbb
Eddie <eddiepulidodev@gmail.com>
2023-07-03 17:36:05 -0700

    July 3rd

---
+++ howHigh.js
@@ -0,0 +1,9 @@
+const howHigh = (node, height = 0) => {
+  if(!node) return height - 1
+  
+  return Math.max(howHigh(node.left, height + 1), howHigh(node.right, height + 1)) 
+};
+
+module.exports = {
+  howHigh,
+};
