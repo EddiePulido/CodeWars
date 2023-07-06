9cc05369757bff7596294ee72b7bfecc72cc887c
Eddie <eddiepulidodev@gmail.com>
2023-07-05 22:28:42 -0700

    July 5th

---
+++ validHKNumber.js
@@ -0,0 +1,14 @@
+// TODO: Define the two functions
+const isValidHKPhoneNumber = num => {
+  const n = '0123456789'
+  const filter = [...num].map((e => n.includes(e) ? '&' : e )).join('')
+  
+  return filter === '&&&& &&&&'
+}
+
+const hasValidHKPhoneNumber = num => {
+  const n = '0123456789'
+  const filter = [...num].map((e => n.includes(e) ? '&' : e )).join('')
+  
+  return filter.includes('&&&& &&&&')
+}
