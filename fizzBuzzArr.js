622d7398beacd4b62c112cb49c17591abdfcf89f
Eddie <eddiepulidodev@gmail.com>
2023-07-29 23:47:03 -0700

    codewars

---
+++ fizzBuzzArr.js
@@ -0,0 +1,12 @@
+function solution(number){
+  const result = [0,0,0]
+  
+  for(let i = 3; i < number; i++){
+    console.log({i, result})
+    if(i % 15 === 0) result[2]++
+    else if(i % 3 === 0) result[0]++
+    else if(i % 5 === 0) result[1]++
+  }
+  
+  return result
+}
