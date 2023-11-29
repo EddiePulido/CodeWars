6773a54a24f4ead60fab7271ec9aaaaa80cf5da9
Eddie <eddiepulidodev@gmail.com>
2023-11-28 17:43:45 -0800

    codewars

---
+++ removeNode.js
@@ -0,0 +1,12 @@
+const removeNode = (head, target, curr = head, prev = null) => {
+  if(curr.val === target){
+    if(prev){
+      prev.next = prev.next.next
+      return head
+    }
+    
+    return head.next
+  }
+  
+  return removeNode(head, target, curr.next, curr)
+};
