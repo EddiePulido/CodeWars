b8008ddcf2407ae228eb3afb5689dba9453942b1
Eddie <eddiepulidodev@gmail.com>
2024-01-27 17:52:31 -0800

    structy

---
+++ sum_possible.py
@@ -0,0 +1,18 @@
+def sum_possible(amount, numbers, memo={}):
+  if amount == 0:
+    return True
+  
+  if amount < 0:
+    return False
+  
+  if amount in memo:
+    return memo[amount]
+  
+  for num in numbers:
+    if sum_possible(amount - num, numbers, memo):
+      memo[amount] = True
+      return True
+    
+  memo[amount] = False
+  
+  return False
