506cc624cc99101454d66b764985d3dd2200dda0
Eddie <eddiepulidodev@gmail.com>
2024-02-17 18:09:42 -0800

    structy

--- connectedComponentsCount.js
+++ connectedComponentsCount.js
@@ -1,26 +1,49 @@
+// const connectedComponentsCount = (graph) => {
+//   const visited = new Set()
+//   let count = 0
+  
+//   for(key in graph){
+//     if(!visited.has(+key)){
+//       const stack = [ +key ]
+      
+//       while(stack.length){
+//         const curr = stack.pop()
+//         visited.add(curr)
+        
+//         for(neighbor of graph[curr]){
+//           if(!visited.has(neighbor)) stack.push(neighbor)
+//         }
+//       }
+      
+//       count++
+//     }
+    
+    
+    
+//   }
+  
+//   return count
+// }
+
 const connectedComponentsCount = (graph) => {
   const visited = new Set()
   let count = 0
   
-  for(key in graph){
-    if(!visited.has(+key)){
-      const stack = [ +key ]
-      
-      while(stack.length){
-        const curr = stack.pop()
-        visited.add(curr)
-        
-        for(neighbor of graph[curr]){
-          if(!visited.has(neighbor)) stack.push(neighbor)
-        }
-      }
-      
+  for(node in graph){
+    if(!visited.has(+node)){
+      exploreComponent(graph, node, visited)
       count++
     }
-    
-    
-    
   }
-  
-  return count
+ 
+ return count
+}
+
+
+const exploreComponent = (graph, node, visited) => {
+ visited.add(+node)
+ 
+ for(n of graph[node]){
+   if(!visited.has(n)) exploreComponent(graph, n, visited)
+ }
 }
