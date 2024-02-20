1ce2a050346ab03944370226d21c318df844abb4
Eddie <eddiepulidodev@gmail.com>
2024-02-19 18:22:12 -0800

    structy

--- largestComponent.js
+++ largestComponent.js
@@ -1,25 +1,60 @@
-const largestComponent = (graph) => {
-  const visited = new Set()
-  let max = 0
+// const largestComponent = (graph) => {
+//   const visited = new Set()
+//   let max = 0
   
-  for(node in graph){
-    max = Math.max(max,exploreNode(graph, node, visited, {c:0}))
-  }
+//   for(node in graph){
+//     max = Math.max(max,exploreNode(graph, node, visited, {c:0}))
+//   }
   
-  return max
-};
+//   return max
+// };
 
-const exploreNode= (graph, curr, visited, count) => {
-  if(visited.has(curr.toString())) return 0
-  count.c++
-  visited.add(curr.toString())
+// const exploreNode= (graph, curr, visited, count) => {
+//   if(visited.has(curr.toString())) return 0
+//   count.c++
+//   visited.add(curr.toString())
   
-  for(neighbor of graph[curr]){
-    exploreNode(graph,neighbor,visited,count)
-  }
+//   for(neighbor of graph[curr]){
+//     exploreNode(graph,neighbor,visited,count)
+//   }
   
-  return count.c
-}
+//   return count.c
+// }
+
+// const largestComponent = (graph) => {
+//   const visited = new Set()
+//   let max = 0
+  
+//   for(node in graph){
+//     if(!visited.has(node)){
+//       let size = 0
+//       const stack = [ node ]
+
+//       while(stack.length){
+  
+//         const curr = stack.pop()
+//         if(!visited.has(curr)){
+//           visited.add(curr)
+//           size++
+
+//           for(neighbor of graph[curr]){
+//             if(!visited.has(neighbor)) stack.push(neighbor)
+//           }
+//         }
+        
+//       }
+
+//       max = Math.max(size, max)
+//     }
+    
+//   }
+  
+//   return max
+// }
+
+// module.exports = {
+//   largestComponent,
+// };
 
 const largestComponent = (graph) => {
   const visited = new Set()
@@ -27,29 +62,25 @@ const largestComponent = (graph) => {
   
   for(node in graph){
     if(!visited.has(node)){
-      let size = 0
-      const stack = [ node ]
-
-      while(stack.length){
-  
-        const curr = stack.pop()
-        if(!visited.has(curr)){
-          visited.add(curr)
-          size++
-
-          for(neighbor of graph[curr]){
-            if(!visited.has(neighbor)) stack.push(neighbor)
-          }
-        }
-        
-      }
-
-      max = Math.max(size, max)
+      max = Math.max(componentSize(graph, node, visited), max)
     }
-    
   }
   
   return max
+}
+
+const componentSize = (graph, node, visited) => {
+  visited.add(node)
+  
+  let size = 1
+  
+  for(n of graph[node]){
+    if(!visited.has(n)){
+      size += componentSize(graph, n, visited)
+    }
+  }
+  
+  return size
 }
 
 module.exports = {
