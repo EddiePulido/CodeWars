8899e728ed48895918181d8f5c505f9f79771b21
Eddie <eddiepulidodev@gmail.com>
2024-03-01 11:02:23 -0800

    codewars

--- frequencySort.js
+++ frequencySort.js
@@ -1,14 +1,20 @@
+// function solve(arr){
+//   const freq = arr.reduce((a,c) => (a[c] = ++a[c] || 1, a), {})
+  
+
+//   arr.sort((a,b) => {
+//     if(freq[a] === freq[b]){
+//       return a - b
+//     }
+    
+//     return freq[b] - freq[a]
+//   })
+  
+//   return arr
+// }
+
 function solve(arr){
   const freq = arr.reduce((a,c) => (a[c] = ++a[c] || 1, a), {})
   
-
-  arr.sort((a,b) => {
-    if(freq[a] === freq[b]){
-      return a - b
-    }
-    
-    return freq[b] - freq[a]
-  })
-  
-  return arr
+  return arr.sort((a,b) => freq[a] === freq[b] ? a - b : freq[b] - freq[a]) 
 }
