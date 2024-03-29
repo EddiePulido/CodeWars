f9ab11354f5ebc04627826cd15107f0eea3517ff
Eddie <eddiepulidodev@gmail.com>
2024-03-29 15:00:53 -0700

    clash of code

---
+++ windowsRooms.js
@@ -0,0 +1,13 @@
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+
+var inputs = readline().split(' ');
+const X = parseInt(inputs[0]);
+const Y = parseInt(inputs[1]);
+
+// Write an answer using console.log()
+// To debug: console.error('Debug messages...');
+
+console.log(X * Y);
