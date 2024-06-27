8b09eec90d3d51c020d6c97253d272151a24531a
Eddie <eddiepulidodev@gmail.com>
2024-06-26 17:04:32 -0700

    codewars

--- lengthOfSequence.js
+++ lengthOfSequence.js
@@ -1,5 +1,6 @@
 const lengthOfSequence = (arr, n) => {
+  const count = arr.reduce((a,c) => a + +(c === n), 0)
   const a = arr.indexOf(n)
   const b = arr.lastIndexOf(n)
-  return a == b ? 0 : arr.slice((a), b + 1).length
+  return a == b || count > 2 ? 0 : arr.slice((a), b + 1).length
 }
