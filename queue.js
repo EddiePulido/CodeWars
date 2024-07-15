d57b733da3b3cbd4c79f22587672c65d358572c9
Eddie <eddiepulidodev@gmail.com>
2024-07-14 22:07:34 -0700

    codewars

---
+++ queue.js
@@ -0,0 +1,40 @@
+function Node (val) {
+  this.val = val
+  this.next = null
+}
+
+function Queue () {
+  this.head = null
+  this.tail = null
+  
+  this.add = function(val){
+    const node = new Node(val)
+    
+    if(!this.head){
+      this.head = node
+      this.tail = node
+    }else{
+      this.tail.next = node
+      this.tail = node
+    }
+  }
+  
+  this.remove = function() {
+    if(this.head){
+      const node = this.head
+      this.head = this.head.next
+      
+      return node.val
+    } 
+  }
+
+}
+
+const queue = new Queue
+
+queue.add(5)
+queue.add(6)
+queue.add(7)
+
+console.log(queue.remove())
+console.log(queue.remove())
