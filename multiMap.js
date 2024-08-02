f92b6e7eeeca4f3a7f4426235d968b2e6d1bd72a
Eddie <eddiepulidodev@gmail.com>
2024-08-02 01:53:45 -0700

    codewars

--- multiMap.js
+++ multiMap.js
@@ -1,11 +1,11 @@
 function multiMap(arrVals, arrCallbacks) {
-	const f = (a,c) => {
-    a[c] = []
-    for(const cb of arrCallbacks){
-      a[c].push(cb(c))
-    }
-    return a
-  }
-  
-  return reduce(arrVals, f, {})
+	const f = (a, c) => {
+		a[c] = []
+		for (const cb of arrCallbacks) {
+			a[c].push(cb(c))
+		}
+		return a
+	}
+
+	return reduce(arrVals, f, {})
 }
