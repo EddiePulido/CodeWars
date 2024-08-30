0fa6f81e06f44013e2aa5f15feba771769d47ecd
Eddie <eddiepulidodev@gmail.com>
2024-08-30 16:05:35 -0700

    codewars

--- hasPath.py
+++ hasPath.py
@@ -7,3 +7,13 @@ def has_path(graph, src, dst):
       return True
     
   return False
+
+def has_path(graph, src, dst):
+  if src == dst:
+    return True
+
+  for neighbor in graph[src]:
+    if has_path(graph, neighbor, dst):
+      return True
+
+  return False
