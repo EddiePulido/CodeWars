6ef59bebd805d1c4ec6c83b2b2505494e54ada81
Eddie <eddiepulidodev@gmail.com>
2024-09-26 14:11:21 -0700

    codewars

--- anagrams.js
+++ anagrams.js
@@ -7,3 +7,19 @@ const anagrams = (s1, s2) => {
   
   return bucket.join('') === '0'.repeat(26)
 }
+
+const anagrams = (s1, s2) => {
+  if(s1.length !== s2.length) return false
+  
+  const arr = new Array(26).fill(0)
+
+  for(let i = 0; i < s1.length; i++){
+    const a = s1[i].charCodeAt() - 97
+    const b = s2[i].charCodeAt() - 97
+    
+    arr[a]++
+    arr[b]--
+  }
+
+  return arr.every(e => e === 0)
+};
